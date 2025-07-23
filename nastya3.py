0952ac192dadaebbe04c382ea2efcc20237190a5
Anastasya2012 <IvStacy2012@yandex.ru>
2025-07-23 12:22:41 +0300

    nastya3

---
+++ nastya3.py
@@ -0,0 +1,31 @@
+'''a= int(input())
+b= int(input())
+if a<=b:
+    print(a)
+if b<a:
+    print(b)
+a= int(input())
+if a > 0:
+    print(1)
+elif a < 0:
+    print(-1)
+else:
+    print(0)'''
+x1= int(input())
+y1= int(input())
+x2= int(input())
+y2= int(input())
+if x1%2==0 and y1%==0 or x1%2!=0 and y1%!=0:
+    print("black")
+        elif x1%2==0 and y1%!=0 or x1%2!=0 and y1%==0:
+            print("white")
+if x2%2==0 and y2%==0 or x2%2!=0 and y2%!=0:
+    print("black")
+        elif x2%2==0 and y1%!=0 or x2%2!=0 and y1%==0:
+            print("white")
+if x1="black" and x2="black":
+    print("YES")
+elif x1="white" and x2="white":
+    print("YES")
+else:
+    print("NO")
