bfe515e5731eb737a686110590b31685eb409752
Anastasya <Ivstacy2012@yandex.ru>
2025-07-23 14:23:35 +0300

    добавила две задачки

--- nastya3.py
+++ nastya3.py
@@ -1,4 +1,5 @@
-'''a= int(input())
+#знак числа
+a= int(input())
 b= int(input())
 if a<=b:
     print(a)
@@ -10,7 +11,7 @@ if a > 0:
 elif a < 0:
     print(-1)
 else:
-    print(0)'''
+    print(0)
 #шахматная доска
 x1 = int(input())
 y1 = int(input())
@@ -18,5 +19,13 @@ x2 = int(input())
 y2 = int(input())
 if (x1 + y1) % 2 == (x2 + y2) % 2:
     print("YES")
+else:
+    print("NO")
+#високостный год
+a = int(input())
+if (a % 400 == 0) or (a % 4 == 0):
+    print("YES")
+elif (a % 100 == 0):
+    print("NO")
 else:
     print("NO")
