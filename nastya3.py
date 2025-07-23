521d7b48cf72dce5060a8a6a16fa9b2e2227470c
Anastasya <Ivstacy2012@yandex.ru>
2025-07-23 14:01:57 +0300

    подписала

--- nastya3.py
+++ nastya3.py
@@ -11,25 +11,12 @@ elif a < 0:
     print(-1)
 else:
     print(0)'''
-x1= int(input())
-y1= int(input())
-x2= int(input())
-y2= int(input())
-if x1%2==0 and y1%==0 or x1%2!=0 and y1%!=0:
-    print("black")
-        elif x1%2==0 and y1%!=0 or x1%2!=0 and y1%==0:
-            print("white")
-if x2%2==0 and y2%==0 or x2%2!=0 and y2%!=0:
-    print("black")
-        elif x2%2==0 and y1%!=0 or x2%2!=0 and y1%==0:
-            print("white")
-if x1="black" and x2="black":
-    print("YES")
-elif x1="white" and x2="white":
+#шахматная доска
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+if (x1 + y1) % 2 == (x2 + y2) % 2:
     print("YES")
 else:
     print("NO")
-
-    34563
-    67890
-    6576565
