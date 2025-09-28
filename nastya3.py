2008e49efe55b1d6eae1a810a0c63e68176f33be
Anastasya <Ivstacy2012@yandex.ru>
2025-09-28 12:24:43 +0300

    update README.md

--- nastya3.py
+++ nastya3.py
@@ -1,51 +1,62 @@
-#задача на минимум
-#правильно
-a = int(input())
-b = int(input())
-if a<=b:
-    print(a)
-if b<a:
-    print(b)
-#знак числа
-#правильно
-a= int(input())
-if a > 0:
-    print(1)
-elif a < 0:
-    print(-1)
-else:
-    print(0)
-#шахматная доска
-#правильно
-x1 = int(input())
-y1 = int(input())
-x2 = int(input())
-y2 = int(input())
-if (x1 + y1) % 2 == (x2 + y2) % 2:
-    print("YES")
-else:
-    print("NO")
-#високостный год
-#неправильно, исправил. сначала проверяем делится ли на 400, если не делится то делится ли на 100, 
-#если не делится то делится ли на 4, и если нет то значит не високосный
-a = int(input())
-#if (a % 400 == 0) or (a % 4 == 0):
-if (a % 400 == 0):
-    print("YES")
-elif (a % 100 == 0):
-    print("NO")
-else:
-    print("NO")
-#яша плавает в бассейне
-N= int(input())
-M= int(input())
-x= int(input())
-y= int(input())
-if y>x:
-    print(x)
-else:
-    print(y)
-#последняя цифра числа
-k = int(input())
-y = k % 10
-print(y)
+'''import turtle
+t=turtle.Pen()
+for x in range(4):
+    t.circle(100)
+    t.left(90)
+import turtle
+t=turtle.Pen()
+for x in range(6):
+    t.circle(100)
+    t.left(60)
+import turtle
+t=turtle.Pen()
+number_of_circles=int(turtle.numinput("количество окружностей","сколько окружностей в вашей розетке?", 6))
+for x in range(number_of_circles):
+    turtle.speed(10)
+    t.circle(100)
+    t.left(360/number_of_circles)
+name=input("как тебя зовут?")
+while name != " ":
+    for x in range(100):
+        print(name, end = " ")
+    print()
+    name=input("введите еще имя или нажмите [Enter], чтобы выйти: ")
+print("💩💩💩💩💩💩💩")
+import turtle
+t=turtle.Pen()
+turtle.bgcolor("black")
+colors = ["red", "yellow", "blue", "green", "orange", "purple", "white", "brown", "gray", "pink"]
+family = turtle.textinput("моя семья", "введите имя или нажмите [enter], чтобы выйти:")
+while name != "":
+    family.append(name)
+    name = turtle.textinput("моя семья", "введите имя или нажмите [Enter], чтобы выйти: ")
+for x in range(100):
+    t.pencolor(colors[x%len(family)])
+    t.penup()
+    t.forward(x*4)
+    t.pendow()
+    t.white(family[x%len(family)], font = ("Arial", int((x+4)/4), "bold"))
+    t.left(360/len(family)+2)
+import turtle
+t=turtle.Pen()
+t.penup()
+turtle.bgcolor("black")
+sides=int(turtle.numinput("количество стороне у вашей спирали (2-6)?", 4, 2, 6))
+colors = ["red", "yellow", "blue", "green", "orange", "purple"]
+for m in range(100):
+    t.forward(m*4)
+    position = t.position()
+    heading = t.heading()
+    for n in range (int(m/2)):
+        t.pendown()
+        t.pencolor(colors[n%siders])
+        t.forward(2*n)
+        t.right(360/sides - 2)
+        t.penup()
+    t.setx(position[0])
+    t.sety(position[1])
+    t.setheading(heading)
+    t.left(360/len(family)+2)'''
+for i in range(4):
+    sl=input
+    print(sl+sl)
